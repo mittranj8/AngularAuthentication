c3e22ae5469ea7736314929848e9af3126cbf43c
Mittranj Pansuriya <mpansuriya@reedassociates.org>
2020-02-05 12:26:45 -0600

    created first page

--- angular-auth-ngrx/src/app/app.module.ts
+++ angular-auth-ngrx/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LandingComponent } from './components/landing/landing.component';
@@ -14,7 +15,13 @@ import { LogInComponent } from './components/log-in/log-in.component';
     LogInComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'log-in', component: LogInComponent },
+      { path: 'sign-up', component: SignUpComponent },
+      { path: '', component: LandingComponent },
+      { path: '**', redirectTo: '/' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
